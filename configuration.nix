25083eaeb5ae0d2d2c181adf9ba0967b72ee2341
Santiago Cézar <santiagocezar2013@gmail.com>
2024-01-23 12:18:45 -0300

    make flatpak apps look even better!

--- configuration.nix
+++ configuration.nix
@@ -133,6 +133,9 @@
     vlc # make gtk flatpaks look at least acceptable
   ];
 
+  # Flatpak stuff
+
+  services.flatpak.enable = true;
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
@@ -141,11 +144,47 @@
     ];
   };
 
+  # begin quote https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1767513263
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+    };
+    aggregatedIcons = pkgs.buildEnv {
+      name = "system-icons";
+      paths = with pkgs; [
+        libsForQt5.breeze-qt5  # for plasma
+        gnome.gnome-themes-extra
+      ];
+      pathsToLink = [ "/share/icons" ];
+    };
+    aggregatedFonts = pkgs.buildEnv {
+      name = "system-fonts";
+      paths = config.fonts.packages;
+      pathsToLink = [ "/share/fonts" ];
+    };
+  in {
+    "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+    "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-emoji
+      noto-fonts-cjk
+    ];
+  };
+  #end quote
+
   # Enable GTK theming for Wayland apps on KDE
   programs.dconf.enable = true;
 
+
   security.polkit.enable = true;
-  services.flatpak.enable = true;
   # Do it here instead of HM to automatically open ports I think
   programs.kdeconnect.enable = true;
   programs.fish.enable = true;
