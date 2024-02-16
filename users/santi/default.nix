803f4878a033494b39c6bbe17b4ccb41a52f61e1
Santiago Cézar <santiagocezar2013@gmail.com>
2024-02-16 18:07:31 -0300

    math was discovered again

--- users/santi/default.nix
+++ users/santi/default.nix
@@ -1,17 +1,4 @@
-)(mathematica.override {
-        source = pkgs.requireFile {
-    name = "Mathematica_XXMathematica_13.3.1_LINUX";
-    # Get this hash via a command similar to this:
-    # nix-store --query --hash \
-    # $(nix store add-path Mathematica_XX.X.X_BNDL_LINUX.sh --name 'Mathematica_XX.X.X_BNDL_LINUX.sh')
-    sha256 = "0000000000000000000000000000000000000000000000000000";
-    message = ''
-      Your override for Mathematica includes a different src for the installer,
-      and it is missing.
-    '';
-    hashMode = "recursive";
-  };
-}{ config, pkgs, inputs,  ... }:
+{ config, pkgs, inputs,  ... }:
 let
   user = "santi";
   home = "/home/${user}";
