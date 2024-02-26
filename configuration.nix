c7cda50e672c36b43066be64027d8d5f76b928c5
Santiago Cézar <santiagocezar2013@gmail.com>
2024-02-26 15:22:43 -0300

    uh yeah

--- configuration.nix
+++ configuration.nix
@@ -50,7 +50,7 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.desktopManager.qtile.enable = true;
+  services.xserver.windowManager.qtile.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
