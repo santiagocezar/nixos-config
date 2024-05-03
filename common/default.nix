bf4db144cc7b4ded22e436834185be125da36947
Santiago Cézar <santiagocezar2013@gmail.com>
2024-05-03 19:29:59 -0300

    my key

--- common/default.nix
+++ common/default.nix
@@ -81,6 +81,7 @@
       # Enable flakes
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
+      trusted-public-keys = [ "e123:yqa6CRpymC7WLiD5pHVrEyqvhXQLSbxl4bOAdlA8/eY=" ];
     };
 
     # https://dataswamp.org/~solene/2022-07-20-nixos-flakes-command-sync-with-system.html
