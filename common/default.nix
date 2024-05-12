805a3c0c4078ddd13b1967aea2441863275d5dcc
Santiago Cézar <santiagocezar2013@gmail.com>
2024-05-12 16:03:58 -0300

    ah true

--- common/default.nix
+++ common/default.nix
@@ -65,6 +65,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [ (import ./overlay.nix) ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- common/overlay.nix
+++ common/overlay.nix
@@ -1,27 +1,29 @@
 final: prev: {
-  obs-studio = final.wrapOBS {
-    plugins = with final.obs-studio-plugins; [
-      obs-vaapi
-    ];
-  };
-  retroarch = prev.retroarch.override {
-    cores = with final.libretro; [
-      genesis-plus-gx
-      snes9x
-      beetle-psx-hw
-    ];
-  };
-  # Add SDL2_gfx to the uh, sandbox? used by `steam-run`
-  steam = prev.steam.override {
-    extraPkgs = pkgs: with pkgs; [
-      SDL2_gfx
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      dejavu_fonts
-    ];
-  };
-  kdePackages.kservice = prev.kdePackages.kservice.overrideAttrs {
-    patches = [./canonical.patch]
-  };
+#   obs-studio = prev.wrapOBS {
+#     plugins = with prev.obs-studio-plugins; [
+#       obs-vaapi
+#     ];
+#   };
+#   retroarch = prev.retroarch.override {
+#     cores = with final.libretro; [
+#       genesis-plus-gx
+#       snes9x
+#       beetle-psx-hw
+#     ];
+#   };
+#   # Add SDL2_gfx to the uh, sandbox? used by `steam-run`
+#   steam = prev.steam.override {
+#     extraPkgs = pkgs: with pkgs; [
+#       SDL2_gfx
+#       noto-fonts
+#       noto-fonts-cjk
+#       noto-fonts-emoji
+#       dejavu_fonts
+#     ];
+#   };
+  kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
+    kservice = kprev.kservice.overrideAttrs {
+      patches = [./canonical.patch];
+    };
+  });
 }
