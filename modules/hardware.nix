550bcdef359423e25f9ee6971956641c8a971477
Santiago Cézar <santiagocezar2013@gmail.com>
2024-05-27 17:00:41 -0300

    this syntax fucking sucks

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -52,10 +52,10 @@
       # Turn media keys into text cursor movement keys
       services.udev.extraHwdb = ''
         evdev:input:b0011v0001p0001*
-        KEYBOARD_KEY_90=home
-        KEYBOARD_KEY_a2=pageup
-        KEYBOARD_KEY_a4=pagedown
-        KEYBOARD_KEY_99=end
+         KEYBOARD_KEY_90=home
+         KEYBOARD_KEY_a2=pageup
+         KEYBOARD_KEY_a4=pagedown
+         KEYBOARD_KEY_99=end
       '';
     }
   ];
