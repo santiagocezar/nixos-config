fb3bb78b2a0deb972190502b0e66e7f63fd1ac4b
Santiago Cézar <santiagocezar2013@gmail.com>
2024-06-01 22:45:22 -0300

    fix proxy maybe idk yet

--- modules/network.nix
+++ modules/network.nix
@@ -56,8 +56,14 @@
           alias = "${pkgs.ariang}/share/ariang/";
         };
         locations."/aria2rpc" = {
-          proxyPass = "http://127.0.0.1:6800";
+          proxyPass = "http://127.0.0.1:6800/jsonrpc";
           proxyWebsockets = true;
+          extraConfig = ''
+            proxy_redirect   off;
+            proxy_set_header X-Real-IP       $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header Host            $host;
+          '';
         };
       };
     };
