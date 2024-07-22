70581cffcbcc2814c38915c8255572682dab8ab4
Santiago Cézar <santiagocezar2013@gmail.com>
2024-07-21 23:52:28 -0300

    refresh, sound option

--- modules/session.nix
+++ modules/session.nix
@@ -36,8 +36,9 @@
     };
 
     # Enable sound with pipewire.
-    sound.enable = true;
+
     hardware.pulseaudio.enable = false;
+
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
