69626815acf4b341b316210154a66833d0fd7e49
Santiago Cézar <santiagocezar2013@gmail.com>
2025-10-12 16:16:10 -0300

    strip path prefix

--- modules/server/http.mod.nix
+++ modules/server/http.mod.nix
@@ -3,12 +3,14 @@
     services.caddy = {
       enable = true;
       virtualHosts."e123.cez.ar".extraConfig = ''
-        handle /aria2/* {
+        handle_path /aria2/* {
           root * "${pkgs.ariang}/share/ariang"
           file_server browse
         }
-        handle /aria2rpc {
-          reverse_proxy :6800/jsonrpc
+        handle_path /aria2rpc {
+          reverse_proxy :6800/jsonrpc {
+            header_down X-Real-IP {http.request.remote}
+          }
         }
       '';
     };
