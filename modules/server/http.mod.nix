202c2bb454f9d868c819ba38aa9c59bbe0b3fcfa
Santiago Cézar <santiagocezar2013@gmail.com>
2025-10-12 16:29:32 -0300

    use default jsonrpc

--- modules/server/http.mod.nix
+++ modules/server/http.mod.nix
@@ -7,10 +7,8 @@
           root * "${pkgs.ariang}/share/ariang"
           file_server browse
         }
-        handle_path /aria2rpc {
-          reverse_proxy :6800/jsonrpc {
-            header_down X-Real-IP {http.request.remote}
-          }
+        reverse_proxy /jsonrpc :6800/jsonrpc {
+          header_down X-Real-IP {http.request.remote}
         }
       '';
     };
