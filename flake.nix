821cd78cfe3c6bba34e8ef1662313da9d25bb5bc
Santiago Cézar <santiagocezar2013@gmail.com>
2025-10-12 13:44:45 -0300

    update lanzaboote part1

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.3.0";
+      url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
