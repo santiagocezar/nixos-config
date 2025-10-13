02388ce7e73a839f19db39f7eb051cce8f759aef
Santiago Cézar <santiagocezar2013@gmail.com>
2025-10-12 21:36:27 -0300

    use the correct download dir

--- modules/server/services.mod.nix
+++ modules/server/services.mod.nix
@@ -16,6 +16,7 @@
     services.aria2 = {
       enable = true;
       rpcSecretFile = "/run/secrets/aria2-rpc-token.txt";
+      settings.dir = "/var/lib/downloads";
     };
     services.tailscale.enable = true;
     services.inadyn = {
