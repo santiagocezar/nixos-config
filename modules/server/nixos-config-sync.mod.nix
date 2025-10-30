5b6d568e5893dd825719c7278e136fdd2827b900
Santiago Cézar <santiagocezar2013@gmail.com>
2025-10-29 22:35:09 -0300

    syntax error

--- modules/server/nixos-config-sync.mod.nix
+++ modules/server/nixos-config-sync.mod.nix
@@ -1,40 +1,41 @@
 {
   _srv.nixos = { config, pkgs, ... }: 
-  let 
-    repoPath = "/etc/nixos";
-    repoRemotePath = "/etc/nixos";
-  in
-    {
-      users.groups."mirrormirror" = {};
-      users.users."mirrormirror" = {
-        group = "mirrormirror";
-        isSystemUser = true;
-      };
-      
-      security.sudoextraRules = [{
-        commands = [
-          {
-            command = "${pkgs.systemd}/bin/systemctl start ${config.systemd.services.nixos-config-sync.name}";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-        groups = [ "wheel" ];
-      }];
+    let 
+      repoPath = "/etc/nixos";
+      repoRemotePath = "/etc/nixos";
+    in
+      {
+        a
+        users.groups."mirrormirror" = {};
+        users.users."mirrormirror" = {
+          group = "mirrormirror";
+          isSystemUser = true;
+        };
+        
+        security.sudoextraRules = [{
+          commands = [
+            {
+              command = "${pkgs.systemd}/bin/systemctl start ${config.systemd.services.nixos-config-sync.name}";
+              options = [ "NOPASSWD" ];
+            }
+          ];
+          groups = [ "wheel" ];
+        }];
 
-      systemd.services.nixos-config-sync =  {
-        description = "Sync NixOS config to GitHub";
-        wants = [ "network-online.target" ];
-        serviceConfig = {
-          Type = "oneshot";
-          Restart = "on-failure";
-          User = "mirrormirror";
-          WorkingDirectory = repoPath;
-          ExecStart = pkgs.writeShellScript "nixos-config-sync.sh" ''
-            #!/bin/sh
-            export GIT_SSH_COMMAND='ssh -i ${config.sops.secrets.ms-token.path}'
-            git push --mirror mirror
-          '';
+        systemd.services.nixos-config-sync =  {
+          description = "Sync NixOS config to GitHub";
+          wants = [ "network-online.target" ];
+          serviceConfig = {
+            Type = "oneshot";
+            Restart = "on-failure";
+            User = "mirrormirror";
+            WorkingDirectory = repoPath;
+            ExecStart = pkgs.writeShellScript "nixos-config-sync.sh" ''
+              #!/bin/sh
+              export GIT_SSH_COMMAND='ssh -i ${config.sops.secrets.ms-token.path}'
+              git push --mirror mirror
+            '';
+          };
         };
       };
-    };
-  }
+}
