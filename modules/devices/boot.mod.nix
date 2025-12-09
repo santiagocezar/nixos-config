24e6052a9ee74df8d2d77948b810a7dfabae63ea
Santiago Cézar <santiagocezar2013@gmail.com>
2025-12-09 10:27:46 -0300

    launcher para reiniciar a Windows

--- modules/devices/boot.mod.nix
+++ modules/devices/boot.mod.nix
@@ -16,42 +16,80 @@ in
     };
   };
 
-  e123.nixos = { pkgs, lib, ... }: {
-    imports = [ lanzaboote.nixosModules.lanzaboote ];
+  e123.nixos =
+    { pkgs, lib, ... }:
+    {
+      imports = [ lanzaboote.nixosModules.lanzaboote ];
 
-    # Lanzaboote currently replaces the systemd-boot module.
-    # This setting is usually set to true in configuration.nix
-    # generated at installation time. So we force it to false
-    # for now.
-    boot.loader.systemd-boot.enable = lib.mkForce false;
+      # Lanzaboote currently replaces the systemd-boot module.
+      # This setting is usually set to true in configuration.nix
+      # generated at installation time. So we force it to false
+      # for now.
+      boot.loader.systemd-boot.enable = lib.mkForce false;
 
-    boot.lanzaboote = {
-      enable = true;
-      pkiBundle = "/etc/secureboot";
+      boot.lanzaboote = {
+        enable = true;
+        pkiBundle = "/etc/secureboot";
+      };
+      environment.systemPackages = [
+        # For debugging and troubleshooting Secure Boot.
+        pkgs.sbctl
+      ];
     };
-    environment.systemPackages = [
-      # For debugging and troubleshooting Secure Boot.
-      pkgs.sbctl
-    ];
-  };
 
-  adachix00.nixos = { pkgs, lib, ... }: {
-    imports = [ lanzaboote.nixosModules.lanzaboote ];
+  adachix00.nixos =
+    { pkgs, lib, ... }:
+    {
+      imports = [ lanzaboote.nixosModules.lanzaboote ];
 
-    environment.systemPackages = [
-      # For debugging and troubleshooting Secure Boot.
-      pkgs.sbctl
-    ];
+      environment.systemPackages = [
+        # For debugging and troubleshooting Secure Boot.
+        pkgs.sbctl
+      ];
 
-    # Lanzaboote currently replaces the systemd-boot module.
-    # This setting is usually set to true in configuration.nix
-    # generated at installation time. So we force it to false
-    # for now.
-    boot.loader.systemd-boot.enable = lib.mkForce false;
+      # Lanzaboote currently replaces the systemd-boot module.
+      # This setting is usually set to true in configuration.nix
+      # generated at installation time. So we force it to false
+      # for now.
+      boot.loader.systemd-boot.enable = lib.mkForce false;
 
-    boot.lanzaboote = {
-      enable = true;
-      pkiBundle = "/var/lib/sbctl";
+      boot.lanzaboote = {
+        enable = true;
+        pkiBundle = "/var/lib/sbctl";
+      };
     };
-  };
+  adachix00.home =
+    { pkgs, lib, ... }:
+    let
+      boot-to-windows = pkgs.writeShellApplication {
+        name = "boot-to-windows";
+        runtimeInputs = with pkgs; [
+          efibootmgr
+          gnugrep
+        ];
+        text = ''
+          # Look up the boot number for Windows in the EFI records
+          boot_number=$(efibootmgr | grep -Po "(?<=Boot)\S{4}(?=( |\* )Windows)")
+
+          # Check that Windows EFI entry was found
+          if [ -z "$boot_number" ]; then
+              >&2 echo "Cannot find Windows boot in EFI, exiting"
+              exit 1
+          fi
+
+          # Set next boot to be Windows and reboot the machine
+          sudo efibootmgr -n "$boot_number" && reboot
+        '';
+      };
+    in
+    {
+      xdg.desktopEntries.boot-to-windows = {
+        name = "Reiniciar a Windows";
+        genericName = "Windows";
+        exec = ''sh -c "SHELL=/bin/sh pkexec ${boot-to-windows}/bin/boot-to-windows"'';
+        terminal = false;
+        icon = "preferences-system-network-share-windows";
+        categories = [ "Utility" ];
+      };
+    };
 }
